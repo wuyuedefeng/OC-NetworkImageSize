b644e6c84644afc37fa7e02b3d4a79642fca6244
wangsen <578595193@qq.com>
2015-06-18 18:06:13 +0800

    统一接口

---
+++ ImageSizeTest/UIImage+itdCategory.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+UIImage_itdCategory.h
+//  ImageSizeTest
+//
+//  Created by TX-009 on 15/6/18.
+//  Copyright (c) 2015年 TX-009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (itdCategory)
+
+- (CGSize)itd_sizeOfImageWithUrlStr:(NSString *)imgUrlStr;
+
+@end
